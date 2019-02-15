2a1e740b87f1c2c3ca3c5eeeac854adf2c9f886a
SaraZ <saraaban@gmail.com>
2019-02-15 13:44:21 -0800

    created board table

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+from flask import Flask, render_template, request, session, redirect
+# from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'oh-so-secret'
+
+app.secret_key = "secret_key"
+# debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
+
+@app.route('/board')
+def load_board():
+    board = boggle_game.make_board()
+    session["board"] = board
+    return   render_template('board.html', board = board)
+    
