0d9582ab8b81a89cbba670769b99bff3eec004c8
SaraZ <saraaban@gmail.com>
2019-02-15 16:04:00 -0800

    show score

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 from flask import Flask, render_template, request, session, redirect, jsonify
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
+# from datetime import timedelta
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'oh-so-secret'
 
 app.secret_key = "secret_key"
-# debug = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
-board = boggle_game.make_board()
 
+# @app.before_request
+# def make_session_permanent():
+#     session.permanent = True
+#     app.permanent_session_lifetime = timedelta(seconds=60)
 
 @app.route('/')
 def load_board():
-    session["board"] = board
-    return render_template('board.html', board = board)
+    session["board"] = boggle_game.make_board()
+    session["score"] = 0
+    # session["max_age"]= 60
+    return render_template('board.html', board = session["board"])
 
 
 @app.route('/wordlist', methods=['POST'])
 def verify_word_in_board():
-    
-    return jsonify({'result': "we made it"})
+    word = request.form['word']
+    game_response = check_word(word)
+    # print("game_response", game_response)
+    if game_response == "ok":
+        session["score"] = session["score"] + len(word)
+    return jsonify({'result': game_response,'score': session['score']})
+
+def check_word(word):  
+    return boggle_game.check_valid_word(session["board"], word)
+
--- static/boggle.js
+++ static/boggle.js
@@ -6,22 +6,28 @@ wait for submit on form, prevent default, AJAX send input value to server
 
 const $form = $("#guess-form")
 const $guessInput =$("#guess-input")
+const $showResult = $("#show-result")
+const $showScore = $("#show-score")
 
 $form.on("submit", async function(evt){
     
     evt.preventDefault();
-    const serverConfirmation = await submitGuess();
-   console.log(serverConfirmation)
+    const guessResponse = await submitGuess();
+    console.log(guessResponse)
+    showResult(guessResponse)
 })
 
-
-
-async function submitGuess(){
-   
+async function submitGuess(){  
     let word = $guessInput.val();
     const response = await $.post("/wordlist", {word})
-
+    console.log("response", response)
     return response;
+}
+
+function showResult(guessResponse){
+   $showResult.html(guessResponse.result)
+   $showScore.html(guessResponse.score)
+//    score = guessResponse.score
 }
 
 
